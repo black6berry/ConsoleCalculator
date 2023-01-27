45300d9151b98b8864888b82b853c8472117d281
MikeVazowskii <mikevazowskii@yandex.ru>
2023-01-27 21:12:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleCalculator/ActionOperator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    internal class ActionOperator
+    {
+        public string a;
+        public string b;
+        public ActionOperator(string a, string b) 
+        {
+            this.a = a;
+            this.b = b;
+        }
+        
+        public void CheckOperator(string action)
+        {
+            try
+            {
+                if(action != null && action == "")
+                {
+                    switch (action)
+                    {
+                        case "*":
+                            Sum(Convert.ToInt32(a), Convert.ToInt32(b));
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+                
+            }
+            catch (Exception ) 
+            {
+                Console.WriteLine("Оператор не выбран! выберите оператор");
+            }
+        }
+
+        static void Sum(int a, int b) 
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Sum(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Sum(float a, float b)
+        {
+            Console.WriteLine(a + b);
+        }
+        int Sum(int[] array)
+        {
+            int sum = 0;
+
+            foreach (int num in array)
+            {
+                sum += num;
+            }
+
+            return sum; //Сумма всех целых чисел в массиве
+        }
+    }
+}
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        public double a {get; set;}
+        public double b {get; set;}
+        public char oper { get; set;}
+        public double total { get; set;}
+        public char repeate { get; set;}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, это приложение - простой калькулятор, который может выполнять операции: сложение, вычитание, умножение, деление\n\n");
+        again:
+            Console.Write("Введи первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введи второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+        
+            Console.Write("Введи оператор: ");
+            char oper = Convert.ToChar(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                double total = a + b;
+                Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+            }
+
+            else if (oper == '-')
+            {
+                double total = a - b;
+                Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+            }
+
+            else if (oper == '*')
+            {
+                double total = a * b;
+                Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+            }
+
+            else if (oper == '/')
+            {
+                double total = a / b;
+                Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор.");
+            }
+
+            Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
+            char repeate = Convert.ToChar(Console.ReadLine());
+
+            if(repeate == 'д')
+            {
+                goto again;
+            }
+            
+
+            //Console.WriteLine($"\nПервое число: {a}\nВторое число: {b}\nОператор: {action}");
+        
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+
+}
