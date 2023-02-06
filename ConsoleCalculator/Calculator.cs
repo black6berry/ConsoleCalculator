948255f408152c408bf94082c3d2d94f7a33cc24
MikeVazowskii <mikevazowskii@yandex.ru>
2023-02-06 21:12:42 +0300

    edit logic, implemented the oop approach

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -6,8 +6,62 @@ using System.Threading.Tasks;
 
 namespace ConsoleCalculator
 {
-    internal class Calculator
+    internal class Calculator : ICalc
     {
+        private double total;
+        public double num1;
+        public double num2;
+        public char oper;
+        public Calculator(double num1, double num2, char oper) 
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.oper = oper;
+        }
+        public void CheckOperator(char oper)
+        {
+            switch (oper) 
+            {
+                case '+':
+                    Sum(num1, num2);
+                    break;
+                case '-':
+                    Subtract(num1, num2);
+                    break;
+                case '*':
+                    Multiplication(num1, num2);
+                    break;
+                case '/':
+                    Division(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Не выбран оператор");
+                    break;
+            }
+        }
+        public void Subtract(double num1, double num2)
+        {
+            total = num1 - num2;
+            Console.WriteLine($"Разность {num1} и {num2} равна {total}");
+        }
+
+        public void Division(double num1, double num2)
+        {
+            total = num1 / num2;
+            Console.WriteLine($"Деление {num1} и {num2} равно {total}");
+        }
+
+        public void Multiplication(double num1, double num2)
+        {
+            total = num1 * num2;
+            Console.WriteLine($"Умножение {num1} и {num2} равно {total}");
+        }
+
+        public void Sum(double num1, double num2)
+        {
+            total = num1 + num2;
+            Console.WriteLine($"Сумма {num1} и {num2} равна {total}");
+        }
 
     }
 }
--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -1,73 +1,48 @@
 ﻿
+using System.Globalization;
 
 namespace ConsoleCalculator
 {
-    class Program
+    class Program : Calculator
     {
-        public double a {get; set;}
-        public double b {get; set;}
-        public char oper { get; set;}
-        public double total { get; set;}
-        public char repeate { get; set;}
-        static void Main(string[] args)
+        public Program(double num1, double num2, char oper) : base(num1, num2, oper)
         {
-            Console.WriteLine("Привет, это приложение - простой калькулятор, который может выполнять операции: сложение, вычитание, умножение, деление\n\n");
-        again:
-            Console.Write("Введи первое число: ");
-            double a = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Введи второе число: ");
-            double b = Convert.ToDouble(Console.ReadLine());
-        
-            Console.Write("Введи оператор: ");
-            char oper = Convert.ToChar(Console.ReadLine());
-
-            if (oper == '+')
-            {
-                double total = a + b;
-                Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
-            }
-
-            else if (oper == '-')
-            {
-                double total = a - b;
-                Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
-            }
-
-            else if (oper == '*')
-            {
-                double total = a * b;
-                Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
-            }
-
-            else if (oper == '/')
-            {
-                double total = a / b;
-                Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
-            }
-            else
-            {
-                Console.WriteLine("Неизвестный оператор.");
-            }
-
-            Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
-            char repeate = Convert.ToChar(Console.ReadLine());
-
-            if(repeate == 'д')
-            {
-                goto again;
-            }
-            
-
-            //Console.WriteLine($"\nПервое число: {a}\nВторое число: {b}\nОператор: {action}");
-        
-
-            Console.ReadKey();
-
 
         }
 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, это приложение - простой калькулятор, который может выполнять операции: сложение, вычитание, умножение, деление\n\n");
+         again:
+            var listNumbers = new List<double>();
+
+            Console.Write("Введи число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            listNumbers.Add(num1);
+
+            Console.Write("Введи число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            listNumbers.Add(num2);
+
+            Console.Write("Введите оператор: ");
+            char oper = Convert.ToChar(Console.ReadLine());
+
+            Calculator calc = new Calculator(num1, num2, oper);
+            calc.CheckOperator(oper);
+
+            //calc.CheckNumber(listNumbers);
+
+            Console.Write("Вы хотите продолжить работу с калькулятором? (д/н) ");
+            char repeate = Convert.ToChar(Console.ReadLine());
+
+            if (repeate == 'д' || repeate == 'Д')
+            {
+                goto again;
+            }
+
+            Console.ReadKey();
+        }
+
+
     }
-
-
 }
